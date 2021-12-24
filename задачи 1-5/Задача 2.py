b6049ebb7cbb5d6230f4f3ee1ca12e8e5df75055
ShadowFiendZXC <96101309+ShadowFiendZXC@users.noreply.github.com>
2021-12-24 09:38:11 +0300

    Update Задача 2.py

--- задачи 1-5/Задача 2.py
+++ задачи 1-5/Задача 2.py
@@ -1,6 +1,5 @@
-if __name__=='__main__':
-    a=input()
-    if (a==a[::-1])==True:
-        print('Палиндром ')
+a=input()
+if (a==a[::-1])==True:
+    print('Палиндром ')
     else:
         print('не Палиндром ') 
